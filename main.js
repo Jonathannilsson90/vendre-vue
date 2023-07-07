fb67b319880d36361006d2da8439ce893315a910
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-07-07 11:53:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+///Runda profilbilder - CSS
+///Namnet, följt av en kontaktlänk som leder till de anställdas e-postadress
+///Utöver det kan du designa sidan hur du vill
+
+
+
+const app = Vue.createApp({
+    data(){
+        return {
+            titel: "anställningsprov",
+            users: []
+        }
+    },
+    methods: {
+        async getData() {
+            try {
+              const response = await axios.get("https://reqres.in/api/users");
+              this.users = response.data.data.map(user => ({
+                id: user.id,
+                email: user.email,
+                firstName: user.first_name,
+                lastName: user.last_name,
+                avatar: user.avatar
+              }));
+            } catch (error) {
+              console.log(error);
+            }
+        }
+    },
+    created(){
+        this.getData();
+    }
+})
